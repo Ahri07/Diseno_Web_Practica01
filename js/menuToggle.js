6022928a7b0753900cef170ffc1b9659e8555e46
ManuelCJ <manuel.cvjd@gmail.com>
2025-02-14 13:09:38 +0100

    Landing Page Responsive

---
+++ js/menuToggle.js
@@ -0,0 +1,13 @@
+let menu = document.querySelector("nav .menu");
+let btnOpenMenu = document.querySelector("#btnOpenMenu");
+let btnCloseMenu = document.querySelector("#btnCloseMenu");
+
+// Abrir el menú
+btnOpenMenu.addEventListener("click", function() {
+    menu.classList.add("show-menu");
+});
+
+// Cerrar el menú
+btnCloseMenu.addEventListener("click", function() {
+    menu.classList.remove("show-menu");
+});
